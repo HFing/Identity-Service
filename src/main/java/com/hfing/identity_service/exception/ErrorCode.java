9a733537327dc774f36ca4ad0728e3db2f6aa3eb
Nguyễn Thái Hoàng <148040890+HFing@users.noreply.github.com>
2025-08-27 23:55:04 +0700

    Exception handling and Response return using HttpCode

--- src/main/java/com/hfing/identity_service/exception/ErrorCode.java
+++ src/main/java/com/hfing/identity_service/exception/ErrorCode.java
@@ -1,28 +1,29 @@
 package com.hfing.identity_service.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
 public enum ErrorCode {
-    INVAID_KEY(1001, "Invalid Key"),
-    UncaughtException(9999, "An unexpected error occurred"),
-    USER_EXISTS(1002, "User already exists"),
-    USERNAME_INVALID(1003, "Username Must be between 4 and 50 characters"),
-    INVAID_PASSWORD(1004, "Password must be between 8 and 20 characters"),
-    USER_NOT_FOUND(1005, "User not found"),
-    UNAUTHENTICATED(1006, "Unauthenticated"),
+    INVAID_KEY(1001, "Invalid Key", HttpStatus.BAD_REQUEST),
+    UncaughtException(9999, "An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_EXISTS(1002, "User already exists", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "Username Must be between 4 and 50 characters", HttpStatus.BAD_REQUEST),
+    INVAID_PASSWORD(1004, "Password must be between 8 and 20 characters", HttpStatus.BAD_REQUEST),
+    USER_NOT_FOUND(1005, "User not found", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1006, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1007, "You do not have permission", HttpStatus.FORBIDDEN),
     ;
 
     private int code;
     private String message;
+    private HttpStatusCode statusCode;
 
-    ErrorCode(int code, String message) {
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
         this.message = message;
+        this.statusCode = statusCode;
     }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public int getCode() {
-        return code;
-    }
 }
